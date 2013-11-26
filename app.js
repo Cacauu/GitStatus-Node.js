e9973759f84a6e865a76039d3cc07f4baedba24c
Cacauu <achternbosch@me.com>
2013-11-26 14:59:08 +0100

    Require requests-module

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var express = require('express');
 var http = require('http');
 var path = require('path');
 var gitstatus = require(__dirname+'/src/gitstatus.js');
+var request = require('request');
 
 var app = express();
 
